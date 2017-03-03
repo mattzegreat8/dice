217598d8edb68f61197dc2cdb6a8f979558e4055
gold <mcavoto@sjnma.org>
2017-03-03 07:02:06 -0600

    [deleted previous structure and started to implement a while loop]

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from random import randrange
 print("This game consists of 2 players.\n Each player will roll the dice and the player that rolled the highest number wins. \n This process will be repeated 3 times.")
-
+'''
 player1= raw_input("What is player ones name?")
 player2= raw_input("What is player twos name?")
 
-'''
+
 roll1=randrange(1,7)
 print("{}, you rolled this!".format(player1))
 print(roll1)
@@ -20,4 +20,12 @@ if(roll1<roll2):
 	print("{} wins".format(player2))
 '''
 
-roll= randrange(1,7)
+player1roll=randrange(1,7)
+player2roll=randrange(1,7)
+
+wincount=3
+player1win=0
+player2win=0
+
+while(player1win != wincount or player2win!= wincount):
+	
