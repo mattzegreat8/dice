7a2def6e14582d49277b168fa515504b33ea106f
gold <mcavoto@sjnma.org>
2017-03-18 22:47:57 -0500

    [updated rule list]

--- modified.py
+++ modified.py
@@ -1,11 +1,16 @@
 from random import randrange
 print("This game consists of 6 different dice and 3 rounds. \n At the beginning of the game, each player will choose a certain number sided dice.")
-print("There will be a 4,6,8,10,12, and 20 sided dice to choose from.")
+print("There will be a 4,8, and 12 sided dice to choose from.")
 player1roll=0
 player2roll=0
 player1win=0
 player2win=0
 wincount=3
-dice= [randrange(1,4), randrange(1,6), randrange(1,8), randrange(1,12), randrange(1,18), randrange(1,20)]
-player1dice= raw_input("What dice do you want?")
-player2dice= raw_input("What dice do you want?")
+dice= [randrange(1,4), randrange(1,8), randrange(1,12)]
+player1dice= int(raw_input("What dice do you want player 1?"))
+player2dice= int(raw_input("What dice do you want player 2?"))
+
+while(player1dice or player2dice != 4 or 8 or 12):
+	print("I'm sorry, we do not have that dice please choose again. \n Your options are 4, 8 , and 12")
+	if(player1dice or player2dice == 1):
+		print("Congrats you won!")
--- readme.md
+++ readme.md
@@ -1,4 +1,4 @@
 1. If you roll a 1 you automatically win.
-2. If you roll the same number two times in a row, you lose.
-3. If one player picks a higher sided dice the other player gets to roll more amount of times depending on what dice they choose.
+2. If the players roll the same number, player 1 wins.
+3. If a player chooses the d12 they lose.
 4. If a player rolls double the other players roll, the player with the double roll loses the game. 
